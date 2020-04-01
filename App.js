9bd7920db36ff4378938a315caf38a8bfeed47bd
vishakha2020 <62323152+vishakha2020@users.noreply.github.com>
2020-04-01 20:24:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -13,6 +13,8 @@ import {
   ScrollView,
   View,
   Text,
+  Button,
+  Image,
   StatusBar,
 } from 'react-native';
 
@@ -24,125 +26,61 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import GetState from "./src/components/GetState";
-import LogModule from "./src/components/Log";
-import AppStorage from "./src/model/Storage";
-import { apiCall } from "./src/common/ApiCaller";
-import DefaultPreference from 'react-native-default-preference'
-import AppConfig from './src/components/AppConfig';
+import ImageCapture from './src/components/ImageCaptureModule';
+import Upload from './src/components/UploadModule';
 
-function App (){
+class App extends React.Component{
 
-  var inputData = {
-    0 : "738097725498,PERIODIC,37.32689395,-122.02685541,3.42,IGN_ON,49,,637175797.6805,,,,,,,,10,,gps",
-    1: "738097725498,PERIODIC,37.32736552,-122.02684942,5.1,IGN_ON,50,,637175813.67904,,,,,,,,10,,gps",
-    2: "738097725498,PERIODIC,37.32746542,-122.01972558,50.37,IGN_ON,91,,637176377.76122,,,,,,,,10,,gps",
-    3: "738097725498,PERIODIC,37.32701219,-122.01972909,55.34,IGN_ON,92,,637176391.85442,,,,,,,,10,,gps",
-    4: "738097725498,PERIODIC,37.32655865,-122.01976278,0,IGN_ON,93,,637176404.83804,,,,,,,,10,,gps",
-    5: "738097725498,PERIODIC,37.3261052,-122.01973702,3.50,IGN_ON,94,,637176418.74622,,,,,,,,10,,gps",
-    6: "738097725498,PERIODIC,37.32563464,-122.01972943,0,IGN_ON,95,,637176431.76022,,,,,,,,10,,gps",
-    7: "738097725498,PERIODIC,37.32515997,-122.01973828,0,IGN_ON,96,,637176445.75428,,,,,,,,5,,gps",
-    8: "738097725498,PERIODIC,37.32469057,-122.01971305,0,IGN_ON,97,,637176469.75323,,,,,,,,5,,gps",
-    9: "738097725498,PERIODIC,37.32470957,-122.02029078,3.27,IGN_ON,98,,637176476.85693,,,,,,,,10,,gps",
-    10: "738097725498,PERIODIC,37.32470383,-122.0208769,5.02,IGN_ON,99,,637176489.85236,,,,,,,,5,,gps",
-    11: "738097725498,PERIODIC,37.32473856,-122.02146999,3.19,IGN_ON,100,,637176505.84044,,,,,,,,10,,gps"
+  constructor(props){
+    super(props);
+    this.state = {
+      filePath: {}
+    };
+  }
+
+  handleCapture = (e) => {
+    e.preventDefault();
+    let resp = ImageCapture();
+    console.log("handle capture response "+JSON.stringify(resp));
+    // this.setState({
+    //   filePath: resp
+    // });
+    // console.log("handle capture "+JSON.stringify(this.state.filePath.uri));
   }
   
-  let i = 0;
-  var str = inputData[0];
-  var prev = str.split(',');
-  var stateData = {state: "StandStill", time: prev[8], speed: prev[4], latitude: prev[2], longitude:prev[3]};
+  handleUpload = (e) => {
+    e.preventDefault();
+    Upload(this.state.filePath.uri, "sample_image_1.jpeg", this.state.filePath.type, "s3");
+  }
 
-  //Vishakha - React hook to execute code in intervals
-  // useEffect(() => {
-  //   const timestamp = setInterval(() => {
-  //     let data = GetState(stateData, inputData[i]);
-  //     let done = JSON.stringify(LogModule("Packet"+i, data, 'object'));
-  //     //console.log("Log Module value is "+ done);
-  //     // AppStorage._retrieveData("Packet"+i).then(function(value){
-  //     //   let dt = new Date(value.time);
-  //     //   dt = dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds();
-  //     //   console.log(dt+":Drive State changes to "+value.state+" at location "+value.latitude+" and "+value.longitude);
-  //     // });
-  //     Promise.resolve(AppStorage._retrieveData("Packet"+i)).then(function(value){
-  //       value = JSON.parse(value);        
-  //       //console.log(value.time);
-  //       // let dt = new Date(value.time);
-  //       // dt = dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds();
-  //       console.log(value.time+":Drive State changes to "+value.state.toUpperCase()+" at location "+value.latitude+" and "+value.longitude);
-  //     });
-      
-  //     i++;
-  //   },1000);
-  //   return () => {
-  //     if(i == 11){
-        
-  //       clearInterval(timestamp);
-  //     }
-  //   }
-  // },[]);
-
-
-    //Vishakha - following code is on how to call a generic function for api call and store in default pref.
-    let postParam = {
-        mobile_uuid: '104744259663407', 
-        fcm_token: 'APA91bFlK9mIL5_aD3VEeiENwhKZGahEEnLvbTuuwTQZsCHNzVdkVU8bCPmd8S8XiYFzOIs8kD4OafjLx87_BLXb45Z8HDPkFk0FQNjUPZTjhZQt_aEh9C4'
-    };
-
-    let url = 'http://api.zoblite.com/zoblite_api/is_asset_registered';
-
-    //AppConfig(url,postParam, "App_Configurations", 'app_config');
-
+  render() {
     return (
-      <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-      </>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          {/* <Image
+            source={{ uri: this.state.filePath.uri }}
+            style={{ width: 250, height: 250 }}
+          />
+          <Text style={{ alignItems: 'center' }}>
+            {this.state.filePath.uri}
+          </Text> */}
+          <Button title="Choose File" onPress={this.handleCapture.bind(this)} />
+          <Button title="Upload File" onPress={this.handleUpload.bind(this)} />
+        </View>
+      </View>
     );
+  }
 }
 
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   engine: {
     position: 'absolute',
--- src/components/ImageCaptureModule.js
+++ src/components/ImageCaptureModule.js
@@ -1,79 +1,31 @@
-import React from 'react';
 import ImagePicker from "react-native-image-picker";
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
-class ImageCapture extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          filePath: {},
-        };
-    }
-
-    chooseFile = () => {    
-        var options = {
-          title: 'Select Image',
-          storageOptions: {
-            skipBackup: true,
-            path: 'images',
-          },
-        };
-        ImagePicker.showImagePicker(options, response => {
-          console.log('Response = ', response);
-    
-          if (response.didCancel) {
-            console.log('User cancelled image picker');
-          } else if (response.error) {
-            console.log('ImagePicker Error: ', response.error);
-          } else if (response.customButton) {
-            console.log('User tapped custom button: ', response.customButton);
-            alert(response.customButton);
-          } else {
-            let source = response;
-            // You can also display the image using data:
-            // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-            this.setState({
-              filePath: source,
-            });
-          }
-        });
+//Vishakha - Module to capture an image from camera roll or phone gallery
+function ImageCapture() {
+  
+    var options = {
+        title: 'Select Image',
+        storageOptions: {
+        skipBackup: true,
+        path: 'images',
+        },
     };
-
-    render() {
-        return (
-          <View style={styles.container}>
-            <View style={styles.container}>
-              {/*<Image 
-              source={{ uri: this.state.filePath.path}} 
-              style={{width: 100, height: 100}} />*/}
-              <Image
-                source={{
-                  uri: 'data:image/jpeg;base64,' + this.state.filePath.data,
-                }}
-                style={{ width: 100, height: 100 }}
-              />
-              <Image
-                source={{ uri: this.state.filePath.uri }}
-                style={{ width: 250, height: 250 }}
-              />
-              <Text style={{ alignItems: 'center' }}>
-                {this.state.filePath.uri}
-              </Text>
-              <Button title="Choose File" onPress={this.chooseFile.bind(this)} />
-            </View>
-          </View>
-        );
-    }
+    ImagePicker.showImagePicker(options, response => {
+        console.log("Response => "+response.uri);
+        if (response.didCancel) {
+            console.log('User cancelled image picker');
+        } else if (response.error) {
+            console.log('ImagePicker Error: ', response.error);
+        } else {
+            return response;
+        // let source = response;
+        // this.setState({
+        //     filePath: source,
+        // });
+        }
+    });
+//02261082022
 
 }
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
 
 export default ImageCapture;
